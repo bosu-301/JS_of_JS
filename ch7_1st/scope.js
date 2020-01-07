224ec5dda68ea3b4ddf096daaecb8523a38a60ec
bosu-301 <43314354+bosu-301@users.noreply.github.com>
2020-01-07 15:59:32 +0900

    Update scope.js

--- ch7_1st/scope.js
+++ ch7_1st/scope.js
@@ -9,10 +9,10 @@ f(5);
 //위 코드에서 x는 함수 f 안에서는 존재하나 그 밖에서는 x를 사용 할 수 없다
 //이때 x의 스코프는 함수f 이다
 
-minyoungjun = (byungsin) =>{
-    if (byungsin>150){
-        byungsin -= 30;
+minyoungjun = (bs) =>{
+    if (bs>150){
+        bs -= 30;
     }
-    console.log(`민영준 키는 ${byungsin}cm 입니다(리얼임) \n`);
+    console.log(`민영준 키는 ${bs}cm 입니다(리얼임) \n`);
 }
 
